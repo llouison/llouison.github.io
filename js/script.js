47adc92a105f56053576d15dfef4923ca3ba9481
lisa louison <lisa.louison@gmail.com>
2017-06-24 22:54:03 -0400

    created hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 console.log('hello');
+
+
+let displayMenu = false;
+
+document.getElementById('menu').addEventListener('click', toggleMenu)
+
+function toggleMenu() {
+    if (displayMenu === true){
+        console.log('hiding menu');
+        document.querySelector('header').setAttribute('class', 'hidden');
+        displayMenu = false;
+    }
+    else if (displayMenu === false){
+        console.log('showing menu');
+        document.querySelector('header').setAttribute('class', 'visible');
+        displayMenu = true;
+    }
+}
